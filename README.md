cebf26650a0d23024986c59a92684103bc179b42
Ponkhy <github@myon.lu>
2024-08-27 13:55:41 +0200

    Init Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Piptime
+
+TBA
+
+---
+## Installation
+### Quick:
+1. Install Tampermonkey | [Google Chrome](https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) | [Firefox](https://addons.mozilla.org/firefox/addon/tampermonkey)
+2. Click [Install](https://github.com/Ponkhy/Piptime/raw/main/piptime.user.js) here
+### Alternative:
+1. Open the **piptime.user.js** script in this repo.
+2. Click the **Raw** button at the top of the file to view it's code
+3. Copy the source code
+4. Open Tampermonkey and click the "Create a new script..." button
+5. Paste the code into the script window and click File -> Save
---
+++ piptime.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Piptime
+// @namespace    https://github.com/Ponkhy/Piptime
+// @version      1.0.0
+// @description  TBA
+// @author       Ponkhy
+// @match        https://www.youtube.com/*
+// @icon         https://www.youtube.com/favicon.ico
+// @downloadURL  https://github.com/Ponkhy/Piptime/raw/main/piptime.user.js
+// @updateURL    https://github.com/Ponkhy/Piptime/raw/main/piptime.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function formatTime(seconds) {
+        const h = Math.floor(seconds / 3600);
+        const m = Math.floor((seconds % 3600) / 60);
+        const s = Math.floor(seconds % 60);
+        return `${h.toString().padStart(2, '0')}:${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
+    }
+
+    function displayRuntime() {
+        const video = document.querySelector('video')
+        const liveBadge = document.querySelector('.ytp-live-badge')
+
+        if (video && liveBadge) liveBadge.innerText = `Live - ${formatTime(video.currentTime)}`
+    }
+
+    setInterval(displayRuntime, 1000);
+})();
